e173f3a157ca582139ce031c442c44183a93ec3e
HongJongMin <gwa7417@gmail.com>
2025-08-10 12:50:19 +0900

    feat: 어노테이션 추가
    [어노테이션을 추가하였습니다.]

--- src/main/java/com/example/gospace/archive/controller/ArchiveController.java
+++ src/main/java/com/example/gospace/archive/controller/ArchiveController.java
@@ -1,9 +1,11 @@
 package com.example.gospace.archive.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 public class ArchiveController {
 
 }
