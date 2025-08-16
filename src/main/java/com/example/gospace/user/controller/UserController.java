04dc0fe105a2ae99d98fd23496b6fd1da3915373
LeeEunJi20 <ellen3228@naver.com>
2025-08-16 18:20:16 +0900

    feat : User 마이페이지 구현 User 엔테티 연결 AnswerEntity 일부 수정

--- src/main/java/com/example/gospace/user/controller/UserController.java
+++ src/main/java/com/example/gospace/user/controller/UserController.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
+@Service
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
