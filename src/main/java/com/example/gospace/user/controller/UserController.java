903ffe8c75d7eaa438ef0050bb52274b6f218aa9
SYDLK <kmj977284@gmail.com>
2025-08-20 13:52:10 +0900

    refactor[user]: 회원가입 api 테스트
    [회원가입 API @RequestBody -> @ModelAttribute로 변경하여 파일 업로드 지원]

--- src/main/java/com/example/gospace/user/controller/UserController.java
+++ src/main/java/com/example/gospace/user/controller/UserController.java
@@ -8,15 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -31,8 +26,8 @@ public class UserController {
         return userService.getUserInfo(userId);
     }
 
-    @PostMapping("/auth/signup")
-    public ResponseEntity<Long> signup(@RequestBody @Valid SignupRequest req) {
+    @PostMapping(value = "/auth/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Long> signup(@ModelAttribute @Valid SignupRequest req) {
         return ResponseEntity.ok(userService.signup(req));
     }
 
